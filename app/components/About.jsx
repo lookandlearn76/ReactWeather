1ed37a8d6e1fed9a8980279c4b5b7ba13f2f6b08
Mark <lookandlearn76@yahoo.co.uk>
2017-06-15 00:22:53 +0100

    changed to scss

--- app/components/About.jsx
+++ app/components/About.jsx
@@ -16,22 +16,22 @@ var About = (props) => {
       <p>Here are some of the tools I used to create it:</p>
       <ul>
         <li>
-          <a href="https://facebook.github.io/react" target="_blank">React</a> - This was the Javascript framework used
+          <a href="https://facebook.github.io/react" target="_blank">React</a> - the Javascript framework used
         </li>
         <li>
-          <a href="http://openweathermap.org" target="_blank">Open Weather Map</a> - I used Open Weather Map to search for weather data by city name
+          <a href="http://openweathermap.org" target="_blank">Open Weather Map</a> - Open Weather Map to search for weather data by city name
         </li>
         <li>
-          <a href="http://foundation.zurb.com/" target="_blank">Foundation</a> - I used Foundation for the styling framework
+          <a href="http://foundation.zurb.com/" target="_blank">Foundation</a> - for the styling framework
         </li>
         <li>
-          <a href="https://webpack.js.org/" target="_blank">Webpack</a> - I used Webpack for npm module bundling during production
+          <a href="https://webpack.js.org/" target="_blank">Webpack</a> - for npm module bundling during production
         </li>
         <li>
-          <a href="https://github.com/" target="_blank">Git and Github </a> were used for version-control
+          <a href="https://github.com/" target="_blank">Git and Github </a> for version-control
         </li>
         <li>
-          <a href="https://www.heroku.com/" target="_blank">Heroku</a> - This app is uses a cloud-based Heroku PaaS server
+          <a href="https://www.heroku.com/" target="_blank">Heroku</a> - a cloud-based PaaS server
         </li>
       </ul>
     </div>
