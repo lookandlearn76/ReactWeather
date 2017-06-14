f647fae8a88d5fa9360b67de5af2727fd6256c41
Mark <lookandlearn76@yahoo.co.uk>
2017-06-14 19:08:54 +0100

    styled the about page

--- app/components/About.jsx
+++ app/components/About.jsx
@@ -10,7 +10,31 @@ var React = require('react');
 
 var About = (props) => {
   return (
-    <h3>About Component</h3>
+    <div>
+      <h1 className="text-center">About</h1>
+      <p>Welcome to this ReactJS weather application</p>
+      <p>Here are some of the tools I used to create it:</p>
+      <ul>
+        <li>
+          <a href="https://facebook.github.io/react" target="_blank">React</a> - This was the Javascript framework used
+        </li>
+        <li>
+          <a href="http://openweathermap.org" target="_blank">Open Weather Map</a> - I used Open Weather Map to search for weather data by city name
+        </li>
+        <li>
+          <a href="http://foundation.zurb.com/" target="_blank">Foundation</a> - I used Foundation for the styling framework
+        </li>
+        <li>
+          <a href="https://webpack.js.org/" target="_blank">Webpack</a> - I used Webpack for npm module bundling during production
+        </li>
+        <li>
+          <a href="https://github.com/" target="_blank">Git and Github </a> were used for version-control
+        </li>
+        <li>
+          <a href="https://www.heroku.com/" target="_blank">Heroku</a> - This app is uses a cloud-based Heroku PaaS server
+        </li>
+      </ul>
+    </div>
   )
 };
 
